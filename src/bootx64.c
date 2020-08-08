9ddc136837bf8aa4f8666a685d89d690e8d0264a
JunTae Son <sonjt0705@hanmail.net>
2020-08-08 12:06:48 +0900

    Modified EOL, because of the bug in output. LF -> CRLF

--- src/bootx64.c
+++ src/bootx64.c
@@ -13,13 +13,13 @@ EFI_STATUS UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
 	gRT = SystemTable->RuntimeServices;
 	Status = UefiInitializeConsole();
 	if (Status != EFI_SUCCESS) return Status;
-	gST->ConOut->OutputString(gST->ConOut, L"Initialized console and services.\n");
+	gST->ConOut->OutputString(gST->ConOut, L"Initialized console and services.\r\n");
 	UefiPrintDecimalInteger(DummyDecimal);
-	gST->ConOut->OutputString(gST->ConOut, L"\nPress keyboard to return.\n");
+	gST->ConOut->OutputString(gST->ConOut, L"\r\nPress keyboard to return.\r\n");
 	do {
 		Status = gST->ConIn->ReadKeyStroke(gST->ConIn, &ShutdownKey);
 	} while (Status == EFI_NOT_READY);
-	gST->ConOut->OutputString(gST->ConOut, L"c2p0ZntjMGxsMzkzXzBmX20zZDFjMW4zXzE1XzBwdGkwbjRsfQ==\n");
+	gST->ConOut->OutputString(gST->ConOut, L"c2p0ZntjMGxsMzkzXzBmX20zZDFjMW4zXzE1XzBwdGkwbjRsfQ==\r\n");
 	gRT->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
 	return EFI_SUCCESS;
 }
