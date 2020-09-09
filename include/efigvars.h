6070a07e55938ae69f3f28b07f82322fd9beeebb
JunTae Son <sonjt0705@hanmail.net>
2020-09-09 17:09:59 +0900

    Added UefiErrorShutdown function. Added UefiInitializeImage function. UefiMain function was modified for testing initialization code. Added global variables in efigvars. Modified ninja build script.

--- include/efigvars.h
+++ include/efigvars.h
@@ -8,3 +8,13 @@ extern EFI_BOOT_SERVICES* BS;
 #define gBS BS
 extern EFI_RUNTIME_SERVICES* RT;
 #define gRT RT
+
+extern EFI_GUID LoadedImageProtocolGuid;
+#define gLoadedImageProtocolGuid LoadedImageProtocolGuid
+extern EFI_GUID DevicePathToTextProtocolGuid;
+#define gDevicePathToTextProtocolGuid DevicePathToTextProtocolGuid
+
+extern EFI_LOADED_IMAGE_PROTOCOL* LoadedImageProtocol;
+#define gLoadedImageProtocol LoadedImageProtocol
+extern EFI_DEVICE_PATH_TO_TEXT_PROTOCOL* DevicePathToTextProtocol;
+#define gDevicePathToTextProtocol DevicePathToTextProtocol
--- src/bootx64.c
+++ src/bootx64.c
@@ -4,17 +4,40 @@ EFI_SYSTEM_TABLE* ST;
 EFI_BOOT_SERVICES* BS;
 EFI_RUNTIME_SERVICES* RT;
 
+EFI_GUID LoadedImageProtocolGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+EFI_GUID DevicePathToTextProtocolGuid = EFI_LOADED_IMAGE_DEVICE_PATH_PROTOCOL_GUID;
+
+EFI_LOADED_IMAGE_PROTOCOL* LoadedImageProtocol;
+EFI_DEVICE_PATH_TO_TEXT_PROTOCOL* DevicePathToTextProtocol;
+
+VOID UefiErrorShutdown() {
+	gRT->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+}
+
+VOID UefiInitializeImage(IN EFI_HANDLE ImageHandle) {
+	EFI_STATUS Status;
+	Status = gBS->LocateProtocol(&gDevicePathToTextProtocolGuid, NULL, (VOID**)&gDevicePathToTextProtocol);
+	if (Status != EFI_SUCCESS) UefiErrorShutdown();
+	Status = gBS->OpenProtocol(
+		ImageHandle, &gLoadedImageProtocolGuid, (VOID**)&gLoadedImageProtocol,
+		ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+	if (Status != EFI_SUCCESS) UefiErrorShutdown();
+}
+
 EFI_STATUS UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable) {
 	EFI_STATUS Status;
 	EFI_INPUT_KEY ShutdownKey;
-	INTN DummyDecimal = -1024;
 	gST = SystemTable;
 	gBS = SystemTable->BootServices;
 	gRT = SystemTable->RuntimeServices;
+	Status = gBS->SetWatchdogTimer(0, 0, 0, NULL);
+	if (Status != EFI_SUCCESS) UefiErrorShutdown();
 	Status = UefiInitializeConsole();
-	if (Status != EFI_SUCCESS) return Status;
+	if (Status != EFI_SUCCESS) UefiErrorShutdown();
 	gST->ConOut->OutputString(gST->ConOut, L"Initialized console and services.\r\n");
-	UefiPrintDecimalInteger(DummyDecimal);
+	UefiInitializeImage(ImageHandle);
+	gST->ConOut->OutputString(gST->ConOut, L"Initialized image and device path.\r\nVENDOR: ");
+	gST->ConOut->OutputString(gST->ConOut, gST->FirmwareVendor);
 	gST->ConOut->OutputString(gST->ConOut, L"\r\nPress keyboard to return.\r\n");
 	do {
 		Status = gST->ConIn->ReadKeyStroke(gST->ConIn, &ShutdownKey);
