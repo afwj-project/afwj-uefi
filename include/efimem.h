043b4d72dd2d23acbf0b1cac0502f1d737f25700
JunTae Son <sonjt0705@hanmail.net>
2021-05-11 14:39:54 +0900

    Added UefiMemCpy function in efimem. Added macro in front of parameters in the following two functions. > UefiMalloc > UefiFree

--- include/efimem.h
+++ include/efimem.h
@@ -2,5 +2,6 @@
 
 #include "efigvars.h"
 
-VOID* UefiMalloc(UINTN Size);
-VOID UefiFree(VOID* Pool);
+VOID* UefiMalloc(IN UINTN Size);
+VOID UefiFree(IN VOID* Pool);
+VOID UefiMemCpy(OUT VOID* Destination, IN CONST VOID* Source, IN UINTN Length);
--- src/efimem.c
+++ src/efimem.c
@@ -1,12 +1,18 @@
 #include "efimem.h"
 
-VOID* UefiMalloc(UINTN Size) {
+VOID* UefiMalloc(IN UINTN Size) {
 	VOID* Pool;
 	EFI_STATUS Status = gBS->AllocatePool(EfiLoaderData, Size, &Pool);
 	if (Status != EFI_SUCCESS) Pool = NULL;
 	return Pool;
 }
 
-VOID UefiFree(VOID* Pool) {
+VOID UefiFree(IN VOID* Pool) {
 	gBS->FreePool(Pool);
+}
+
+VOID UefiMemCpy(OUT VOID* Destination, IN CONST VOID* Source, IN UINTN Length) {
+	UINT8* DestinationRaw = (UINT8*)Destination;
+	UINT8* SourceRaw = (UINT8*)Source;
+	while (Length--) *DestinationRaw++ = *SourceRaw++;
 }
