9c1857e45b3bc9e1b288a99b89e5365d68739580
JunTae Son <sonjt0705@hanmail.net>
2021-07-25 18:41:08 +0900

    EFIMEM UPDATE Added EFI_MEMORY_TUPLE type. Added memory pool table. Added three functions. > UefiMemInit > UefiRealloc > UefiGetMemSize Modified two functions for handling memory pool table. > UefiMalloc > UefiFree

--- include/efi/efimem.h
+++ include/efi/efimem.h
@@ -2,6 +2,14 @@
 
 #include "efigvars.h"
 
+typedef struct {
+	UINT64 Size;
+	VOID* Ptr;
+} EFI_MEMORY_TUPLE;
+
+VOID UefiMemInit();
 VOID* UefiMalloc(IN UINTN Size);
+EFI_STATUS UefiRealloc(IN OUT VOID** Pool, IN UINTN Size);
+UINT64 UefiGetMemSize(IN VOID* Pool, OUT EFI_STATUS* StatusRef);
 VOID UefiFree(IN VOID* Pool);
 VOID UefiMemCpy(OUT VOID* Destination, IN CONST VOID* Source, IN UINTN Length);
--- src/efi/efimem.c
+++ src/efi/efimem.c
@@ -1,13 +1,64 @@
 #include "efi/efimem.h"
 
+EFI_MEMORY_TUPLE PoolTable[2048];
+
+VOID UefiMemInit() {
+	for (UINTN i = 0; i < 2048; i++) {
+		PoolTable[i].Size = 0;
+		PoolTable[i].Ptr = NULL;
+	}
+}
+
 VOID* UefiMalloc(IN UINTN Size) {
 	VOID* Pool;
 	EFI_STATUS Status = gBS->AllocatePool(EfiLoaderData, Size, &Pool);
 	if (Status != EFI_SUCCESS) Pool = NULL;
+	for (UINTN i = 0; i < 2048; i++) {
+		if (PoolTable[i].Ptr == NULL && PoolTable[i].Size == 0) {
+			PoolTable[i].Size = Size;
+			PoolTable[i].Ptr = Pool;
+			break;
+		}
+	}
 	return Pool;
 }
 
+EFI_STATUS UefiRealloc(IN OUT VOID** Pool, IN UINTN Size) {
+	VOID* Temp = *Pool;
+	EFI_STATUS Status = gBS->AllocatePool(EfiLoaderData, Size, Pool);
+	if (Status != EFI_SUCCESS) return Status;
+	UINT64 TempSize = UefiGetMemSize(Temp, &Status);
+	if (Status != EFI_SUCCESS) return Status;
+	UefiMemCpy(*Pool, Temp, TempSize);
+	for (UINTN i = 0; i < 2048; i++) {
+		if (PoolTable[i].Ptr == Temp && PoolTable[i].Size == TempSize) {
+			PoolTable[i].Size = Size;
+			PoolTable[i].Ptr = *Pool;
+			break;
+		}
+	}
+	return EFI_SUCCESS;
+}
+
+UINT64 UefiGetMemSize(IN VOID* Pool, OUT EFI_STATUS* StatusRef) {
+	for (UINTN i = 0; i < 2048; i++) {
+		if (PoolTable[i].Ptr == Pool) {
+			*StatusRef = EFI_SUCCESS;
+			return PoolTable[i].Size;
+		}
+	}
+	*StatusRef = EFI_NOT_FOUND;
+	return 0;
+}
+
 VOID UefiFree(IN VOID* Pool) {
+	for (UINTN i = 0; i < 2048; i++) {
+		if (PoolTable[i].Ptr == Pool) {
+			PoolTable[i].Size = 0;
+			PoolTable[i].Ptr = NULL;
+			break;
+		}
+	}
 	gBS->FreePool(Pool);
 }
 
