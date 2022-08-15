d691b0cd34d7b649e93d626133ddd82d0e53eb22
JunTae Son <sonjt0705@hanmail.net>
2022-08-15 19:11:26 +0900

    Added page bit and mapping table. Added two functions in efimem. > IdentityMap4kb > SetupPaging

--- include/efi/efimem.h
+++ include/efi/efimem.h
@@ -5,6 +5,14 @@
 #define UefiMemCpy gBS->CopyMem // ALIAS: Memory Copy Function
 #define UefiMemSet gBS->SetMem // ALIAS: Memory Set Function
 
+#define PAGE_BIT_P (1 << 0)
+#define PAGE_BIT_RW (1 << 1)
+#define PAGE_BIT_US (1 << 2)
+#define PAGE_BIT_A (1 << 5)
+#define PAGE_BIT_D (1 << 6)
+#define PAGE_XD_NX (1 << 63)
+#define PAGE_ADDR_MASK 0x000FFFFFFFFFF000
+
 typedef struct {
 	UINT64 Size;
 	VOID* Ptr;
@@ -18,8 +26,15 @@ typedef struct {
 	UINT32 DescriptorVersion;
 } EFI_MEMORY_MAP;
 
+#pragma pack(push, 1)
+typedef struct {
+	UINT64 Entries[512];
+} MAPPING_TABLE;
+#pragma pack(pop)
+
 extern EFI_MEMORY_MAP MemoryMap;
 extern UINT64 NextAllocPage;
+extern MAPPING_TABLE Pml4;
 
 VOID UefiMemInit();
 VOID* UefiMalloc(IN UINTN Size);
@@ -27,3 +42,6 @@ EFI_STATUS UefiRealloc(IN OUT VOID** Pool, IN UINTN Size);
 UINT64 UefiGetMemSize(IN VOID* Pool, OUT EFI_STATUS* StatusRef);
 VOID UefiFree(IN VOID* Pool);
 VOID* UefiAllocPage();
+VOID IdentityMap4kb(UINT64 Logical);
+extern VOID LoadPml4(MAPPING_TABLE* Pml4);
+VOID SetupPaging();
--- src/efi/efimem.c
+++ src/efi/efimem.c
@@ -4,6 +4,9 @@ EFI_MEMORY_TUPLE PoolTable[2048];
 EFI_MEMORY_MAP MemoryMap;
 UINT64 NextAllocPage;
 
+__attribute__((aligned(4096)))
+MAPPING_TABLE Pml4;
+
 VOID UefiMemInit() {
 	for (UINTN i = 0; i < 2048; i++) {
 		PoolTable[i].Size = 0;
@@ -68,4 +71,44 @@ VOID* UefiAllocPage() {
 	VOID* Page = (VOID*)NextAllocPage;
 	NextAllocPage += 4096;
 	return Page;
+}
+
+VOID IdentityMap4kb(UINT64 Logical) {
+	INT32 Flags = PAGE_BIT_P | PAGE_BIT_RW | PAGE_BIT_US;
+	INT32 Pml4Index = (Logical >> 39) & 0x1FF;
+	INT32 PdpIndex = (Logical >> 30) & 0x1FF;
+	INT32 PdIndex = (Logical >> 21) & 0x1FF;
+	INT32 PtIndex = (Logical >> 12) & 0x1FF;
+	INT32 PoIndex = Logical & 0x7FF;
+	UINT64 PdptAlloc;
+	UINT64 PdtAlloc;
+	UINT64 PtAlloc;
+	if (!(Pml4.Entries[Pml4Index] & PAGE_BIT_P)) {
+		PdptAlloc = (UINT64)UefiAllocPage();
+		for (UINTN i = 0; i < 4096; i++) ((UINT8*)PdptAlloc)[i] = 0;
+		Pml4.Entries[Pml4Index] = (PdptAlloc & PAGE_ADDR_MASK) | Flags;
+		IdentityMap4kb(PdptAlloc);
+	}
+	MAPPING_TABLE* Pdpt = (MAPPING_TABLE*)(Pml4.Entries[Pml4Index] & PAGE_ADDR_MASK);
+	if (!(Pdpt->Entries[PdpIndex] & PAGE_BIT_P)) {
+		PdtAlloc = (UINT64)UefiAllocPage();
+		for (UINTN i = 0; i < 4096; i++) ((UINT8*)PdtAlloc)[i] = 0;
+		Pdpt->Entries[PdpIndex] = (PdtAlloc & PAGE_ADDR_MASK) | Flags;
+		IdentityMap4kb(PdtAlloc);
+	}
+	MAPPING_TABLE* Pdt = (MAPPING_TABLE*)(Pdpt->Entries[PdpIndex] & PAGE_ADDR_MASK);
+	if (!(Pdt->Entries[PdIndex] & PAGE_BIT_P)) {
+		PtAlloc = (UINT64)UefiAllocPage();
+		for (UINTN i = 0; i < 4096; i++) ((UINT8*)PtAlloc)[i] = 0;
+		Pdt->Entries[PdIndex] = (PtAlloc & PAGE_ADDR_MASK) | Flags;
+		IdentityMap4kb(PtAlloc);
+	}
+	MAPPING_TABLE* Pt = (MAPPING_TABLE*)(Pdt->Entries[PdIndex] & PAGE_ADDR_MASK);
+	if (!(Pt->Entries[PtIndex] & PAGE_BIT_P)) Pt->Entries[PtIndex] = (Logical & PAGE_ADDR_MASK) | Flags;
+}
+
+VOID SetupPaging() {
+	for (UINTN i = 0; i < 4096; i++) ((UINT8*)&Pml4)[i] = 0;
+	for (UINTN i = 0; i < 134217728; i += 4096) IdentityMap4kb(i);
+	LoadPml4(&Pml4);
 }
