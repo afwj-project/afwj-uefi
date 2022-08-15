8fba75de5fc03ef70dc167c9ca4b3a5ef238a052
JunTae Son <sonjt0705@hanmail.net>
2022-08-15 19:17:12 +0900

    Added build script for linker and assembler. Python 3.x virtual environment is required since this commit.

---
+++ build/asselect.py
@@ -0,0 +1,24 @@
+#!python
+
+'''
+Python 3.x virtual environment required.
+'''
+
+from sys import argv
+from sys import exit as sexit
+from os import name, system
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        print('Arguments missed!')
+        sexit(2)
+    args = ' '.join(argv[1:])
+    if name == 'nt':
+        system(f'as {args}')
+        sexit(0)
+    elif name == 'posix':
+        system(f'x86_64-w64-mingw32-as {args}')
+        sexit(0)
+    else:
+        print('Unsupported operating system!')
+        sexit(1)
